eda68a31583d011993abdad1ae661afb0b900c57
Leonard Pohl <leonard.pohl@maibornwolff.de>
2021-10-26 10:14:42 +0200

    Updates

---
+++ precache-manifest.de043c344ba2e166c9cc77261000f1e9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d07d1a0f61feb96ca2d0fc2d832a85aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58d62b32915617272bdf",
+    "url": "/static/css/main.61ea77ed.chunk.css"
+  },
+  {
+    "revision": "e6e026de19d9366d746c",
+    "url": "/static/js/2.7411d4bb.chunk.js"
+  },
+  {
+    "revision": "119ff7b0e6dbb852f63360f69938dd73",
+    "url": "/static/js/2.7411d4bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ca59ed2288d647377aa",
+    "url": "/static/js/3.487774d5.chunk.js"
+  },
+  {
+    "revision": "58d62b32915617272bdf",
+    "url": "/static/js/main.eaf8e5d6.chunk.js"
+  },
+  {
+    "revision": "be7e1328218e8a8d27d7",
+    "url": "/static/js/runtime-main.3be6de5e.js"
+  },
+  {
+    "revision": "530410dd6a3722cc734bc6b512db1d26",
+    "url": "/static/media/window-cats.530410dd.jpg"
+  }
+]);
